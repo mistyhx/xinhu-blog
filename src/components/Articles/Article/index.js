ace2acea4e1acf04e7aa7ddc3463729d9e9589a5
xin.hu <xin.hu@xinhus-MBP.fios-router.home>
2020-07-14 17:01:02 -0400

    add basic layout to detail page

--- src/components/Articles/Article/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Article = () => {
-  return (
-    <div>
-      <div>Book</div>My article
-    </div>
-  )
-}
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,17 +1,30 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import SideBar from "../components/SideBar"
 
 export default function BlogTemplate({ data }) {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+    <Layout>
+      <SEO />
+      <SideBar>
+        <h2 style={{ marginBottom: `3.5rem` }}>👀 Read More</h2>
+        Some Other Articles lalallal
+        <ul>
+          <li>I am a list</li>
+        </ul>
+      </SideBar>
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <h5>{frontmatter.date}</h5>
+          <h1>{frontmatter.title}</h1>
+          <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 export const pageQuery = graphql`
