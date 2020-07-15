b5af818d31584bdd79270675aad30e836342c629
xin.hu <xin.hu@xinhus-MBP.fios-router.home>
2020-07-14 22:34:49 -0400

    added line grid to the layout

--- src/components/Articles/ArticlePreview/index.js
+++ src/components/Articles/ArticlePreview/index.js
@@ -2,22 +2,26 @@ import React from "react"
 import { Link } from "gatsby"
 import "./index.css"
 
-const ArticlePreview = ({ data }) => {
+const ArticlePreview = ({ data, order }) => {
+  console.log(order)
   return (
-    <div className="preview-container">
-      <div className="preview-date">
-        <p>{data.frontmatter.date}</p>
-      </div>
-      <div className="preview-title">
-        <Link to={data.frontmatter.title}>
-          <h2>{data.frontmatter.title}</h2>
-          <div className="underline" style={{ backgroundColor: `var(--underlineColor)` }} />
-        </Link>
-      </div>
+    <div className="preview-container" style={{ flexDirection: `${order % 2 == 0 && "row-reverse"}` }}>
+      <div className="preview-info" style={{ borderWidth: `${order % 2 != 0 && "0"}` }}>
+        <div className="preview-date">
+          <p>{data.frontmatter.date}</p>
+        </div>
+        <div className="preview-title">
+          <Link to={data.frontmatter.title}>
+            <h3>{data.frontmatter.title}</h3>
+            <div className="underline" style={{ backgroundColor: `var(--underlineColor)` }} />
+          </Link>
+        </div>
 
-      <div className="preview-excerpt">
-        <p>{data.frontmatter.description}</p>
+        <div className="preview-excerpt">
+          <p>{data.frontmatter.description}</p>
+        </div>
       </div>
+      <div className="preview-empty" style={{ borderWidth: `${order % 2 == 0 && "0"}` }}></div>
     </div>
   )
 }
